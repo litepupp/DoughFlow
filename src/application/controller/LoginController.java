15801f5c77aa3259a4b7008534fc95340c9741f2
Michael Maldonado <michaelmaldonado463@gmail.com>
2020-11-24 01:53:23 -0600

    Beginnings of Login Controller Added code from lab4 solution (how info is passed from View controller to another)

--- src/application/controller/LoginController.java
+++ src/application/controller/LoginController.java
@@ -1,6 +1,43 @@
 package application.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
 public class LoginController {
-	//Test
-	//Test2
+	@FXML
+	private Scene personnelScene;
+	@FXML
+	private Label ErrorLabel;
+	@FXML
+	private PasswordField PasswordField;
+	@FXML
+	private TextField UsernameField;
+	
+public void handle(ActionEvent event) throws IOException {
+		
+		String username = UsernameField.getText();
+		String password = PasswordField.getText();
+		
+		System.out.println(username);
+		System.out.println(password);
+		
+	}
+
+	/*
+	public void setPersonnelScene(Scene scene) {
+		personnelScene = scene;
+		
+	}
+	*/
 }
