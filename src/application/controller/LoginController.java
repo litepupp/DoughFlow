2aee89c9dad91b12881661d5494c5d09d36047ff
Michael Maldonado <michaelmaldonado463@gmail.com>
2020-11-24 01:58:25 -0600

    More work on login

--- src/application/controller/LoginController.java
+++ src/application/controller/LoginController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -23,6 +24,8 @@ public class LoginController {
 	private PasswordField PasswordField;
 	@FXML
 	private TextField UsernameField;
+	@FXML
+	private Button LoginButton;
 	
 public void handle(ActionEvent event) throws IOException {
 		
