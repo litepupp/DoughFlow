8cf705314a1c1c3d92b8ffac8e9c269581e12f29
Michael Maldonado <michaelmaldonado463@gmail.com>
2020-11-25 22:44:16 -0600

    more delete changes

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,13 +6,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	
 	private Stage primaryStage;
-	private BorderPane mainLayout;
 
 	@Override
 	public void start(Stage primaryStage) throws IOException {
